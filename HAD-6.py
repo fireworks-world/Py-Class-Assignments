bb2d9c77ee7df5e4594030d92e55d71f6c46d106
FW-workspace <erikii2019@outlook.com>
2020-08-21 22:17:51 +0300

    HAD

--- HAD-6.py
+++ HAD-6.py
@@ -1,2 +1,17 @@
-print("hello")
-#ALL ASSIGNMENTS DONE
+class MyClass(object):
+    def __init__(self, x):
+        self.x = x
+        self._locked = True
+    def __setattr__(self, name, value):
+        if self.__dict__.get("_locked", False) and name == "x":
+            raise AttributeError("MyClass does not allow assignment to .x member")
+        self.__dict__[name] = value
+
+>>> m = MyClass(3)
+>>> m.x
+3
+>>> m.x = 4
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "<stdin>", line 7, in __setattr__
+AttributeError: MyClass does not allow assignment to .x member
